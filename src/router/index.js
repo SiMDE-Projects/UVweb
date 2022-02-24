7bda6f8daa2076749bd626632e9140982054a874
Armand Ribault <ribaultarmand@gmail.com>
2022-02-24 22:24:47 +0100

    add UvView to router

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,15 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: function () {
       return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
+    },
+  },
+  {
+    path: "/uv/:name",
+    name: "uv",
+    component: function () {
+      return import(/* webpackChunkName: "uv" */ "../views/UvView.vue");
     },
   },
 ];
