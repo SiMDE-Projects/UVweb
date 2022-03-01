b352ae031e1bb7a7813cb2f2aaab3c16c90ffa42
Armand Ribault <ribaultarmand@gmail.com>
2022-03-01 15:45:09 +0100

    setup default PostView posting system

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="post">
     <h1>This is a post page</h1>
+    <form v-on:submit.prevent="submitForm">
+      <AppTextArea
+        name="post"
+        id="post"
+        placeholder="Ton avis..."
+        v-model="form.comment"
+      />
+      <button>Valider</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+const baseURL = "http://localhost:3000/comments";
+
+import AppTextArea from "@/components/AppTextArea.vue";
 export default {
   name: "PostView",
+  components: {
+    AppTextArea,
+  },
+  data() {
+    return {
+      form: {
+        comment: "",
+      },
+    };
+  },
+  methods: {
+    debug() {
+      console.log(this.form.comment);
+    },
+    submitForm() {
+      axios
+        .post(baseURL, this.form)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
 };
 </script>
+
+<style lang="sass" scoped>
+#post
+  resize: none
+</style>
