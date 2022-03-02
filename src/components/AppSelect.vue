a95967c71df14b9ca0bf12bc96327645f87a239d
Armand Ribault <ribaultarmand@gmail.com>
2022-03-02 18:20:42 +0100

    add AppSelect.vue

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="dropdown">
+      <div class="option" v-for="(index, option) in options" :key="index">
+        {{ option.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSelect",
+  props: {
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+</style>
