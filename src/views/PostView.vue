0d0260915fe829989dc733be62468dfaf8389025
Armand Ribault <ribaultarmand@gmail.com>
2022-03-02 17:29:16 +0100

    add precisions for posting on PostView.vue

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="post">
-    <h1>This is a post page</h1>
+    <h1>
+      This is a post page for
+      {{
+        $route.params.name ? $route.params.name.toUpperCase() : "an unknown UV"
+      }}
+    </h1>
     <form v-on:submit.prevent="submitForm">
       <AppTextArea
         name="post"
@@ -8,6 +13,15 @@
         placeholder="Ton avis..."
         v-model="form.comment"
       />
+      <div class="precisions">
+        <font-awesome-icon icon="info-circle" />
+        Ton avis sera soumis à modération une fois posté. Il est possible
+        d'éditer un avis après soumission, peu importe qu'il ait déjà été validé
+        par un modérateur ou non.
+        <em class="text-error"
+          >N'oublie pas que les propos injurieux sont proscrits sur UVweb.</em
+        >
+      </div>
       <button>Valider</button>
     </form>
   </div>
@@ -48,7 +62,16 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-#post
-  resize: none
+<style lang="scss" scoped>
+#post {
+  resize: none;
+}
+
+.precisions {
+  color: gray;
+
+  .text-error {
+    color: orangered;
+  }
+}
 </style>
