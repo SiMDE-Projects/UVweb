ed53b131a6e5e5d8a7190b3a5757b323fa77df0b
Armand Ribault <ribaultarmand@gmail.com>
2022-03-04 15:44:48 +0100

    add AppButton.vue

---
+++ src/components/AppButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="button">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
